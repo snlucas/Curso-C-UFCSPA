4163a9465949bbdce0ac2402294637465f9186e5
Lucas S Nogueira <lucas.lambda.101@gmail.com>
2017-08-05 15:46:02 -0300

    Rascunho do algoritmo

--- algoritmo-numerico-aula-01.c
+++ algoritmo-numerico-aula-01.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <math.io>
 
 int main(){
+    float a, b, c;
 
-    
+    printf("\nDigite o valor de a: \n");
+    scanf("%.2f", &a);
+
+    printf("\nDigite o valor de b: \n");
+    scanf("%.2f", &b);
+
+    printf("\nDigite o valor de c: \n");
+    scanf("%.2f", &c);
+
+    float D = ((b * b) - (4 * a * c));
+
+    if(D >= 0){
+        float x1, x2;
+
+        x1 = (-b+sqrt(D))/2*a;
+        x2 = (-b-sqrt(D))/2*a;
+
+        printf("\nValor de X1: %.2f", x1);
+        printf("\nValor de X2: %.2f", x2);
+    }
+
+    printf("\nRaizes Imaginarias.");
+
     return 0;
 }
